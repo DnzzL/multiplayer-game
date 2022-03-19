766a61f6a7975f8a53187c4e8c4771ca1a5d8cbf
Thomas Legrand <legrand.thomas5@hotmail.fr>
2022-03-19 19:36:36 +0100

    remove comments

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -16,49 +16,6 @@ const io = new Server(server, {
 
 game(io)
 
-// app.get('/', (req, res) => {
-//   res.send('<h1>Hello world</h1>');
-// });
-
-// io.on("connection", (socket) => {
-//   console.log("%s has connected", socket.data.userName)
-//   const users = [];
-//   for (let [id, socket] of io.of("/").sockets) {
-//     users.push({
-//       userID: id,
-//       userName: socket.data.userName,
-//     });
-//   }
-//   socket.emit("users", users);
-// });
-
-// io.use((socket, next) => {
-//   const userName = socket.handshake.auth.userName;
-//   if (!userName) {
-//     return next(new Error("invalid username"));
-//   }
-//   socket.data.userName = userName;
-//   next();
-// });
-
-// io.on("connection", (socket) => {
-//   // notify existing users
-//   socket.broadcast.emit("user connected", {
-//     userID: socket.id,
-//     userName: socket.data.userName,
-//   });
-// });
-
-
-// io.on("players", (players: Player[]) => {
-//   console.log(players)
-// })
-
-// io.on("gameconfig", (gameConfig: GameConfig) => {
-//   console.log(gameConfig)
-// })
-
-
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
